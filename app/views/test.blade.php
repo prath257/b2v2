4efff2f848577a38c7805ea55d9572d99c061532
ksjoshi88 <ksjoshi88@gmail.com>
2014-11-13 11:47:50 +0530

    test

--- app/views/test.blade.php
+++ app/views/test.blade.php
@@ -1,78 +1,133 @@
+<!DOCTYPE html>
 <html>
 <head>
+  	<title>BBarters</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
+    <!--css includes.-->
+    <link rel="shortcut icon" href="{{asset('Images/icons/logo.JPG')}}">
+    <link href="{{asset('css/bootstrapIndex.css')}}" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap-social.css')}}" rel="stylesheet">
+    <link href="{{asset('css/bootstrapValidator.css')}}" rel="stylesheet">
+	<link href="{{asset('css/pages/index.css')}}" rel="stylesheet" type="text/css" >
+    <link href="{{asset('css/WPModal.css')}}" rel="stylesheet">
+    <!--css includes end here.-->
     <style>
 
-        .list li
-        {
-            background-color:#000000;
-            text-align: center;
-            color: #ffffff;
-            opacity: 0.5;
+    #logo3 {
+        font-size: 96px;
+        font-family: "segoe UI";
+        color:#000033;
+        text-align: center;
+    }
+    .letter3 {
+        top: -webkit-calc(50% - 60px);
+        top: calc(50% - 60px);
+        text-shadow: 0px 0px 1px dark grey;
+        float: left;
+    }
+    .letter3:nth-child(1) {
+        -webkit-animation: fade 4s infinite 100ms;
+        animation: fade 4s infinite 100ms;
+        text-shadow: 1px 1px 1px green;
+        color: green
+    }
+    .letter3:nth-child(2) {
+        -webkit-animation: fade 4s infinite 200ms;
+        animation: fade 4s infinite 200ms;
+    }
+    .letter3:nth-child(3) {
+        -webkit-animation: fade 4s infinite 300ms;
+        animation: fade 4s infinite 300ms;
+    }
+    .letter3:nth-child(4) {
+        -webkit-animation: fade 4s infinite 400ms;
+        animation: fade 4s infinite 400ms;
+    }
+    .letter3:nth-child(5) {
+        -webkit-animation: fade 4s infinite 500ms;
+        animation: fade 4s infinite 500ms;
+    }
+    .letter3:nth-child(6) {
+        -webkit-animation: fade 4s infinite 600ms;
+        animation: fade 4s infinite 600ms;
+    }
+    .letter3:nth-child(7) {
+        -webkit-animation: fade 4s infinite 700ms;
+        animation: fade 4s infinite 700ms;
+    }
 
+    .letter3:nth-child(8) {
+        -webkit-animation: fade 4s infinite 800ms;
+        animation: fade 4s infinite 800ms;
+    }
+    .letter3:nth-child(9) {
+            -webkit-animation: fade 4s infinite 900ms;
+            animation: fade 4s infinite 900ms;
         }
+
+.letter3:nth-child(10) {
+        -webkit-animation: fade 4s infinite 1000ms;
+        animation: fade 4s infinite 1000ms;
+    }
+
+.letter3:nth-child(11) {
+        -webkit-animation: fade 4s infinite 1100ms;
+        animation: fade 4s infinite 1100ms;
+    }
+
+.letter3:nth-child(12) {
+        -webkit-animation: fade 4s infinite 1200ms;
+        animation: fade 4s infinite 1200ms;
+    }
+
+    @-webkit-keyframes fade {
+        25% {
+            opacity: 0.2;
+        }
+    }
+
     </style>
-
-
 </head>
+<!--on hovering anywhere on the page, the default text 'Be-Barters is a social shaing website...' is shown.-->
 <body>
 
 
+    <br>
+    <br>
+    <br>
+<br>
+  <br>
+    <br>
+    <br>
+<br>
+    <div class="container">
 
-<div class="container">
+        <div id="carousel1" class="carousel slide carousel-fade col-lg-8">
+            <div class="carousel-inner" style="padding-top:100px; margin-left: 270px">
+                <a id="logo3" class="navbar-brand logo">
+                    <span class='letter3'>B</span>
+                    <span class='letter3'>B</span>
+                    <span class='letter3'>a</span>
+                    <span class='letter3'>r</span>
+                    <span class='letter3'>t</span>
+                    <span class='letter3'>e</span>
+                    <span class='letter3'>r</span>
+                    <span class='letter3'>s</span>
+                    <span class='letter3'>.</span>
+                    <span class='letter3'>c</span>
+                    <span class='letter3'>o</span>
+                    <span class='letter3'>m</span>
+                </a>
+                <div class="col-lg-12">&nbsp;</div>
+                <div class="col-lg-12 tagline" style="font-size: 20px">&nbsp;<i>Don't be users, be barters.</i></div>
+                <div class="col-lg-12">&nbsp;</div>
 
+                <br>
+             </div>
 
-    <div class="row">
-
-        <div class="cbl-lg-12">
-
-            <div class="info" >
-                <ul class="list">
-                </ul>
-            </div>
-
-            <form id="forming" method="post">
-                Username:<input type="text" id="username"><br>
-                Password:<input type="text" id="password"><br>
-                <input type="submit"  value="Login">
-
-            </form>
-
-
-        </div>
-
-    </div>
-
-</div>
-
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-
-<script  >
-
-    $(document).ready(function(){
-
-        var info=$('.info');
-
-        $('#forming').submit(function(e){
-
-            e.preventDefault();
-
-            $.post('/check',{data: 'Hola'},function(data)
-            {
-                alert(data.success);
-                info.hide().find('ul').empty();
-
-                info.find('ul').append('<li>'+'Hello'+'</li>')
-                //thinking of animating
-                info.slideDown();
-            });
-
-        });
-
-    });
-
-</script>
-
-
+       </div>
+       </div>
 </body>
 </html>
