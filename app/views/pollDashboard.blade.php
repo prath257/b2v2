cb625ecef80c7a1c11ba86e461cd732c4a91bc16
prateekkish <prateekkish@gmail.com>
2014-11-20 15:22:51 +0530

    conflict res

--- app/views/pollDashboard.blade.php
+++ app/views/pollDashboard.blade.php
@@ -265,23 +265,23 @@
 </div>
 
 <!-- Modal to show the purchase content -->
-
 <div class="modal fade" id="resultModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Poll Result</h4>
+            </div>
+            <div class="modal-body" id="pollResult">
 
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Poll Result</h4>
-                </div>
-                <div class="modal-body" id="pollResult">
-
-                </div>
             </div>
         </div>
 
-</div>
 
+
+    </div>
+
+</div>
 
 <input type="hidden" id="refreshed" value="no">
 <script src="{{asset('js/reload.js')}}"></script>
